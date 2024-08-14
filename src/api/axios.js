de8141e1bf996f230be2d39e42325fc4d664e48c
lsmin3388 <lsmin3388_@naver.com>
2024-08-15 03:01:10 +0700

    fix: Modify axios setting

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://kpcowell.site/api/v1',
-  baseURL: 'http://localhost/api/v1',
+  baseURL: 'https://kpcowell.site/api/v1',
+  // baseURL: 'http://localhost/api/v1',
   timeout: 20000,
   withCredentials: true,
   headers: {
