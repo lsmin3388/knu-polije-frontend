278bdbe658c95ba6e9f08fe28eeeeb342569fdee
lsmin3388 <lsmin3388_@naver.com>
2024-08-14 15:11:09 +0700

    fix: Modify axios basic host

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost/api/v1',
+  baseURL: 'http://kpcowell/api/v1',
   timeout: 10000,
   withCredentials: true,
   headers: {
